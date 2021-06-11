99cdafe204b976306a449203959c76df8e61662a
Udo Steinberg <udo@hypervisor.org>
2021-06-11 14:03:25 +0200

    x86: Modernized memory map.

--- inc/x86_64/memory.hpp
+++ inc/x86_64/memory.hpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2024 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -20,29 +21,49 @@
 
 #pragma once
 
+#include "macros.hpp"
+
+#define LOAD_ADDR       0x400000
+
 #define PTE_BPL         9
 #define PAGE_BITS       12
-#define PAGE_SIZE       (1 << PAGE_BITS)
-#define PAGE_MASK       (PAGE_SIZE - 1)
-
-#define LOAD_ADDR       0x200000
-
-#define USER_ADDR       0x00007ffffffff000
-#define LINK_ADDR       0xffffffff81000000
-#define CPU_LOCAL       0xffffffffbfe00000
-#define SPC_LOCAL       0xffffffffc0000000
-
-#define OFFSET          (LINK_ADDR - LOAD_ADDR)
-
-#define CPU_GLOBL_DATA  (CPU_LOCAL - 0x1000000)
-
-#define CPU_LOCAL_STCK  (SPC_LOCAL - PAGE_SIZE * 3)
-#define CPU_LOCAL_APIC  (SPC_LOCAL - PAGE_SIZE * 2)
-#define CPU_LOCAL_DATA  (SPC_LOCAL - PAGE_SIZE * 1)
-
-#define SPC_LOCAL_IOP   (SPC_LOCAL)
-#define SPC_LOCAL_IOP_E (SPC_LOCAL_IOP + PAGE_SIZE * 2)
-#define SPC_LOCAL_REMAP (SPC_LOCAL_OBJ - 0x1000000)
-#define SPC_LOCAL_OBJ   (END_SPACE_LIM - 0x20000000)
+#define PAGE_SIZE(L)    BITN ((L) * PTE_BPL + PAGE_BITS)
+#define OFFS_MASK(L)    (PAGE_SIZE (L) - 1)
 
 #define END_SPACE_LIM   (~0UL + 1)
+#define MMAP_SPC_OBJ    (END_SPACE_LIM - 0x20000000)
+
+// Space-Local Area           [--PTE--]---      // ^39 ^30 ^21 ^12
+#define MMAP_SPC_PIO_E  0xffffffffc0002000      // 511 511 000 002
+#define MMAP_SPC_PIO    0xffffffffc0000000      // 511 511 000 000
+#define MMAP_SPC        0xffffffffc0000000      // 511 511 000 000
+
+// CPU-Local Area             [--PTE--]---      // ^39 ^30 ^21 ^12
+#define MMAP_CPU_DATA   0xffffffffbffff000      // 511 510 511 511    4K
+#define MMAP_CPU_DSTT   0xffffffffbff87000      // 511 510 511 391  Data Stack Top
+#define MMAP_CPU_DSTB   0xffffffffbff86000      // 511 510 511 390  Data Stack Base
+#define MMAP_CPU_ISTT   0xffffffffbff85000      // 511 510 511 389  Intr Stack Top
+#define MMAP_CPU_ISTB   0xffffffffbff84000      // 511 510 511 388  Intr Stack Base
+#define MMAP_CPU_DSHT   0xffffffffbff82ff8      // 511 510 511 387  Data Shadow Stack Token
+#define MMAP_CPU_DSHB   0xffffffffbff82000      // 511 510 511 386  Data Shadow Stack Base
+#define MMAP_CPU_ISHT   0xffffffffbff80ff8      // 511 510 511 385  Intr Shadow Stack Token
+#define MMAP_CPU_ISHB   0xffffffffbff80000      // 511 510 511 384  Intr Shadow Stack Base
+#define MMAP_CPU_APIC   0xffffffffbff00000      // 511 510 511 256    4K
+#define MMAP_CPU        0xffffffffbfe00000      // 511 510 511 000    2M
+
+// Global Area                [--PTE--]---      // ^39 ^30 ^21 ^12
+#define MMAP_GLB_MAP1   0xffffffffbe800000      // 511 510 500 000    4M + gap
+#define MMAP_GLB_MAP0   0xffffffffbe000000      // 511 510 496 000    4M + gap
+#define MMAP_GLB_APIC   0xffffffffbd000000      // 511 510 488 000   16M
+#define MMAP_GLB_UART   0xffffffffbc000000      // 511 510 480 000   16M
+#define MMAP_GLB_SMMU   0xffffffffb8000000      // 511 510 448 000   64M
+#define LINK_ADDR       0xffffffff81000000
+
+#define MMAP_GLB_CPUS   0xffffffff40000000      // 511 509 000 000    1G (262144 CPUs)
+#define MMAP_GLB_PCIE   0xffffffc000000000      // 511 256 000 000
+#define MMAP_GLB_PCIS   0xffffff8000000000      // 511 000 000 000  256G (1024 PCI Segment Groups)
+#define BASE_ADDR       0xffffff8000000000      // 511 000 000 000
+
+#define USER_ADDR       0x00007ffffffff000
+
+#define OFFSET          (LINK_ADDR - LOAD_ADDR)
